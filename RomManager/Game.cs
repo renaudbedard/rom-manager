7e98b7736b7412edcf320a79fb9c12df7e83d9e7
Renaud Bédard <renaud.bedard@gmail.com>
2016-07-17 22:01:38 -0400

    OOPSIE DAISY

---
+++ RomManager/Game.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace RomManager
+{
+    class Game
+    {
+        public int Id;
+        public string Name;
+        public string Url;
+        public string Summary;
+        public string Storyline;
+        public int Collection;
+        public float Rating;
+        [JsonProperty("rating_count")]
+        public int RatingCount;
+        public Image[] Screenshots;
+        public Image Cover;
+    }
+
+    class Image
+    {
+        [JsonProperty("cloudinary_id")]
+        public string CloudinaryId;
+        public int Width;
+        public int Height;
+
+        [JsonIgnore]
+        public BitmapImage BitmapImage;
+    }
+}
--- RomManager/MainWindow.xaml.cs
+++ RomManager/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using unirest_net.http;
 
 namespace RomManager
 {
@@ -23,6 +24,36 @@ namespace RomManager
         public MainWindow()
         {
             InitializeComponent();
+            TestAPI();
+        }
+
+        public void TestAPI()
+        {
+            var requestURI = string.Format(
+                "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search={0}&fields={1}&limit={2}",
+                Uri.EscapeDataString("a link to the past"), // search
+                Uri.EscapeDataString("*"), // fields
+                1); // limit
+
+            HttpResponse<Game[]> response = Unirest.get(requestURI)
+                .header("X-Mashape-Key", "Xfb4BtFk3RmshL3HnA1fgXXPEyGEp16mndvjsntX7K4pclsD7h")
+                .header("Accept", "application/json")
+                .asJson<Game[]>();
+
+            var games = response.Body;
+            foreach (var game in games)
+            {
+                // resolve cover image
+                if (game.Cover != null)
+                    game.Cover.BitmapImage = new BitmapImage(new Uri(string.Format("https://res.cloudinary.com/igdb/image/upload/t_{0}/{1}.jpg", "cover_big_2x", game.Cover.CloudinaryId)));
+
+                // resolve screenshots
+                foreach (var screenshot in game.Screenshots)
+                    screenshot.BitmapImage = new BitmapImage(new Uri(string.Format("https://res.cloudinary.com/igdb/image/upload/t_{0}/{1}.jpg", "screenshot_big", screenshot.CloudinaryId)));
+            }
+
+            image.Source = games[0].Cover.BitmapImage;
         }
     }
 }
+
